8ee55db1eb5e88520d431162b1747fc513581f3c
romain.quinio <romain.quinio@gmail.com>
2016-03-13 17:23:33 +0100

    Specify encoding (windows-1252) when reading ck2 files

--- ck2xtext.tests/src/ck2xtext/tests/util/FileResourceHelper.java
+++ ck2xtext.tests/src/ck2xtext/tests/util/FileResourceHelper.java
@@ -4,24 +4,32 @@ import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
 
 import org.eclipse.emf.ecore.resource.Resource;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.xtext.junit4.util.ResourceHelper;
+import org.eclipse.xtext.resource.XtextResource;
 
+/**
+ * Load a CK2 file as a Resource
+ */
 public class FileResourceHelper extends ResourceHelper {
 
 	public Resource resource(File file) throws IOException {
 		InputStream stream = new FileInputStream(file);
-		ResourceSet resourceSet = createResourceSet();
-		Resource resource = resource(stream, computeUnusedUri(resourceSet), null, resourceSet);
-		stream.close();
-		return resource;
+		return resource(stream);
 	}
 
 	public Resource resource(InputStream stream) throws IOException {
 		ResourceSet resourceSet = createResourceSet();
-		Resource resource = resource(stream, computeUnusedUri(resourceSet), null, resourceSet);
+
+		// CK2 files are encoded with windows-1252 (ANSI) encoding
+		Map<String, String> options = new HashMap<String, String>();
+		options.put(XtextResource.OPTION_ENCODING, "windows-1252");
+
+		Resource resource = resource(stream, computeUnusedUri(resourceSet), options, resourceSet);
 		stream.close();
 		return resource;
 	}
